87f83c8a87d92ca6a21c04096c4194194d9c5cda
lutharsanen <lutharsanen.kunam@uzh.ch>
2021-05-02 09:50:41 +0200

    updated cottontaildb initialisation files

--- cottontaildb/cottontaildb_init.py
+++ cottontaildb/cottontaildb_init.py
@@ -1,45 +1,23 @@
-from cottontaildb_client import CottontailDBClient, Type, Literal, column_def
+from cottontaildb_client import CottontailDBClient, Type, Literal, column_def, float_vector
+from PIL import Image
+import numpy as np
 
 with CottontailDBClient('localhost', 1865) as client:
     # Create schema
-    client.create_schema('example_schema')
+    client.create_schema('test_schema')
     # Define entity feature vector columns
     feature_vector_columns = [
         column_def('keyframe_id', Type.INTEGER, nullable=False),
-        column_def('feature_vector', Type.INTEGER, nullable=True),
-        column_def('video_id', Type.INTEGER, nullable=False)
+        column_def('feature_vector', Type.FLOAT_VEC, nullable=True, length=4)
     ]
     # Create entity feature vector
-    client.create_entity('tal_schema', 'tal_feature_vector', feature_vector_columns)
+    client.create_entity('test_schema', 'test_feature_vector', feature_vector_columns)
 
-    # Define entity color columns
-    color_columns = [
-        column_def('keyframe_id', Type.INTEGER, nullable=False),
-        column_def('feature_vector', Type.INTEGER, nullable=True),
-        column_def('video_id', Type.INTEGER, nullable=False)
-    ]
-
-    # Create entity color
-    client.create_entity('tal_schema', 'tal_color',color_columns)
     
-    # Define entity object detection columns
-    feature_vector_columns = [
-        column_def('keyframe_id', Type.INTEGER, nullable=False),
-        column_def('objects', Type.INTEGER, nullable=True),
-        column_def('video_id', Type.INTEGER, nullable=False)
-    ]
+    # Read in an image
+    img = Image.open("test.jpg")
+    # Get a vector from img2vec, returned as a torch FloatTensor
+    #test_vector = img2vec.get_vec(img)
 
-
-    """
-    # Insert entry
-    entry = {'id': Literal(stringData='test_1'), 'value': Literal(intData=1)}
-    client.insert('example_schema', 'example_entity', entry)
-    # Insert batch
-    batch = [
-        ('example_schema', 'example_entity', {'id': Literal(
-            stringData='test_10'), 'value': Literal(intData=10)}),
-        ('example_schema', 'example_entity', {'id': Literal(
-            stringData='test_20'), 'value': Literal(intData=20)})
-    ]
-    client.insert_batch(batch)
-    """
+    details = client.get_entity_details('test_schema', 'test_feature_vector')
+    print(details)
