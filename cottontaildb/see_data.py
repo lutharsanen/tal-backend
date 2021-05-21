967a7fc383bac7e8e75eb14c6a95fce520e04c65
amohap <71710151+amohap@users.noreply.github.com>
2021-05-21 09:45:30 +0200

    mass push

--- cottontaildb/see_data.py
+++ cottontaildb/see_data.py
@@ -10,6 +10,6 @@ with CottontailDBClient('localhost', 1865) as client:
     print(client.get_entity_details("tal_db", "text_search"))
     print(client.get_entity_details("tal_db", "video_tags"))
     
-    #result = client.select("tal_db", "sketch",["box_id","video_id", "keyframe_id"])
-    #result = MessageToDict(list(result)[0])
-    #print(result)
+    result = client.select("tal_db", "color_image",["video_id", "keyframe_id"])
+    result = MessageToDict(list(result)[0])
+    print(result)
