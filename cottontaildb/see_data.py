e15fb693c3a7467caa13ba0c5738e8888c48e1ab
lutharsanen <lutharsanen.kunam@uzh.ch>
2021-05-22 09:22:42 +0200

    added text_search columns

--- cottontaildb/see_data.py
+++ cottontaildb/see_data.py
@@ -33,11 +33,11 @@ with CottontailDBClient('localhost', 1865) as client:
     print(client.get_entity_details("tal_db", "text_search"))
     print(client.get_entity_details("tal_db", "video_tags"))
     print(client.get_entity_details("tal_db", "video_search"))"""
-    print(client.get_entity_details("tal_db", "color_image"))
+    print(client.get_entity_details("tal_db", "video_search"))
     
-    result = client.select("tal_db", "color_image",["video_id", "keyframe_id"])
-    result = MessageToDict(list(result)[0])
-    print(result)
+    #result = client.select("tal_db", "video_search",["video_id"])
+    #result = MessageToDict(list(result)[0])
+    #print(result)
 '''
 
     # Define entity tag columns
@@ -82,4 +82,3 @@ with CottontailDBClient('localhost', 1865) as client:
 
     result = df_new[df_new["count"] >= 2].sort_values(by=['count'],ascending=False)
 '''
-    #print(result)
