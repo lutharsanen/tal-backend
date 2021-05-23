4e54f5b6f01b48ee05aebc1fda698139b0fe2e0a
amohap <71710151+amohap@users.noreply.github.com>
2021-05-23 13:27:41 +0200

    push data from text_search to DB

--- cottontaildb/text_search.py
+++ cottontaildb/text_search.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 # If you don't have tesseract executable in your PATH, include the following:
 # pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
 
-#tess.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+tess.pytesseract.tesseract_cmd = r"C:\Users\fabia\Desktop\SOLIDWORKS_2016_SP5.0\inspection\CommonAppData\SOLIDWORKS\SOLIDWORKS Inspection 2016 Standalone\TrainableOCR\tesseract.exe"
 
 def get_all_filesname(path):
     files = os.listdir(path)
@@ -26,54 +26,64 @@ def cleanhtml(raw_html):
 
 
 def run(path):
-    video_filelist = sorted(get_all_filesname(f"{path}/keyframes_filtered"))[:30]
-
+    video_filelist = sorted(get_all_filesname(f"{path}/keyframes_filtered"))[86:]
+    
     for videonr in tqdm(video_filelist):
         #f = open(f"D:\\Video Retrieval System\\info\\{videonr}.json")
-        f = open(f"{path}/info/{videonr}.json")
-        data = json.load(f)
+        try:
+            f = open(f"{path}/info/{videonr}.json")
+            data = json.load(f)
 
-        with CottontailDBClient('localhost', 1865) as client:    
-            entry = {
-                'video_id': Literal(stringData = videonr),
-                'title': Literal(stringData = data["title"].lower()),
-                'description': Literal(stringData = cleanhtml(data["description"]).lower())
-            }
-            client.insert('tal_db', 'video_search', entry)
-
-            for tag in data["tags"]:
+            with CottontailDBClient('localhost', 1865) as client:    
                 entry = {
                     'video_id': Literal(stringData = videonr),
-                    'tags':Literal(stringData = tag.lower())            
+                    'title': Literal(stringData = data["title"].lower()),
+                    'description': Literal(stringData = cleanhtml(data["description"]).lower())
                 }
-                client.insert('tal_db', 'video_tags', entry)
-            f = open(f"{path}/msb/{videonr}.tsv")
-            start_time = pd.read_csv(f, delimiter="\t")
+                client.insert('tal_db', 'video_search', entry)
 
-            for filename in tqdm(get_all_filesname(f"{path}/keyframes_filtered/{videonr}")):
-                keyframe_id = get_keyframe_id(filename, videonr,path)
-                keyframe_nr = int(keyframe_id)-1
-                image = f"{path}/keyframes_filtered/{videonr}/{filename}"
-                with open(image,"rb") as file:
-                    file_form = {"image": (image, file,"image/png")}
-                    text_url = "http://localhost:5000/model/predict"
-                    r = requests.post(url = text_url, files = file_form)
-                    response = r.json()
-                    capture_text = response["predictions"][0]["caption"].lower()
-                img = Image.open(image)
-                text = tess.image_to_string(img).strip("\n\x0c")
-                if text != (" " or "") and len(text) > 0:
-                    text = text.replace("/n", " ").lower()
+                for tag in data["tags"]:
                     entry = {
                         'video_id': Literal(stringData = videonr),
-                        'keyframe_id': Literal(intData=int(keyframe_id)),
-                        'tesseract_text': Literal(stringData = text),
-                        'start_time':Literal(intData = int(start_time.iloc[keyframe_nr]["startframe"])),
-                        'image_capture_text':Literal(stringData = capture_text)
+                        'tags':Literal(stringData = tag.lower())            
                     }
-                    client.insert('tal_db', 'text_search', entry)
+                    client.insert('tal_db', 'video_tags', entry)
+                f = open(f"{path}/msb/{videonr}.tsv")
+                start_time = pd.read_csv(f, delimiter="\t")
+
+                for filename in tqdm(get_all_filesname(f"{path}/keyframes_filtered/{videonr}")):
+                    if filename != "Thumbs.db" and filename!= ".DAV":
+                        keyframe_id = get_keyframe_id(filename, videonr,path)
+                        keyframe_nr = int(keyframe_id)-1
+                        image = f"{path}/keyframes_filtered/{videonr}/{filename}"
+                        with open(image,"rb") as file:
+                            file_form = {"image": (image, file,"image/png")}
+                            text_url = "http://localhost:5000/model/predict"
+                            r = requests.post(url = text_url, files = file_form)
+                            response = r.json()
+                            capture_text = response["predictions"][0]["caption"].lower()
+                        img = Image.open(image)
+                        text = tess.image_to_string(img).strip("\n\x0c")
+                        if text != (" " or "") and len(text) > 0:
+                            text = text.replace("/n", " ").lower()
+                            entry = {
+                                'video_id': Literal(stringData = videonr),
+                                'keyframe_id': Literal(intData=int(keyframe_id)),
+                                'tesseract_text': Literal(stringData = text),
+                                'start_time':Literal(intData = int(start_time.iloc[keyframe_nr]["startframe"])),
+                                'image_capture_text':Literal(stringData = capture_text)
+                            }
+                            client.insert('tal_db', 'text_search', entry)
+        except OSError:
+            continue
+        except UnicodeDecodeError:
+            continue
+        except UnicodeEncodeError:
+            continue
+        except UnicodeError:
+            continue
 
 #path = "/run/user/1000/gvfs/dav:host=tal.diskstation.me,port=5006,ssl=true"
-path = "/media/lkunam/Elements/Video Retrieval System"
-
+#path = "/media/lkunam/Elements/Video Retrieval System"
+path = "Y:/TAL"
 run(path)
