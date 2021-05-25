2422c98226586b258a346cf7b1140ee010e3e8c6
amohap <71710151+amohap@users.noreply.github.com>
2021-05-25 18:53:36 +0200

    insert first 20 text entries

--- cottontaildb/text_search.py
+++ cottontaildb/text_search.py
@@ -26,7 +26,7 @@ def cleanhtml(raw_html):
 
 
 def run(path):
-    video_filelist = sorted(get_all_filesname(f"{path}/keyframes_filtered"))[:5]
+    video_filelist = sorted(get_all_filesname(f"{path}/keyframes_filtered"))[5:20]
 
     for videonr in tqdm(video_filelist):
         #f = open(f"D:\\Video Retrieval System\\info\\{videonr}.json")
