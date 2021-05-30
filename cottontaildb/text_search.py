aacab0acbc0fb9d9cb754deb88d2a4332b65b23a
amohap <71710151+amohap@users.noreply.github.com>
2021-05-30 11:53:10 +0200

    DB push data text_search (350:500)

--- cottontaildb/text_search.py
+++ cottontaildb/text_search.py
@@ -26,7 +26,7 @@ def cleanhtml(raw_html):
 
 
 def run(path):
-    video_filelist = sorted(get_all_filesname(f"{path}/keyframes_filtered"))[250:350]
+    video_filelist = sorted(get_all_filesname(f"{path}/keyframes_filtered"))[350:500]
 
     for videonr in tqdm(video_filelist):
         #f = open(f"D:\\Video Retrieval System\\info\\{videonr}.json")
